29af34d4eb63242fe76980e5bbd688deaeed8dec
nickarocho <nick@boldlyfine.com>
2022-12-09 15:35:44 -0800

    convert border to SVG circle (pre-animate)

--- 01-pomodoro-timer/src/App.tsx
+++ 01-pomodoro-timer/src/App.tsx
@@ -7,11 +7,27 @@ function App() {
 
   return (
     <main className="App">
-      <div className="timerWrapper flex flex-col justify-center">
-        <div id="clock">15:00</div>
-        <div id="controls">
-          <button id="btnStartStop">START</button>
-          <button id="btnSettings" className="gearIcon my-0 mx-auto" />
+      <div className="outerWrapper relative flex justify-center items-center">
+        <svg
+          className="base-timer__svg z-50"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g className="base-timer__circle">
+            <circle
+              className="base-timer__path-elapsed"
+              cx="50"
+              cy="50"
+              r="45"
+            />
+          </g>
+        </svg>
+        <div className="timerWrapper flex flex-col justify-center">
+          <div id="clock">15:00</div>
+          <div id="controls">
+            <button id="btnStartStop">START</button>
+            <button id="btnSettings" className="gearIcon my-0 mx-auto" />
+          </div>
         </div>
       </div>
     </main>
